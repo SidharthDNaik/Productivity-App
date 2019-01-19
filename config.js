9989063f45498029556c8c7da470ce225a11daba
Kartikeya Agrawal <KartikeyaAgrawal@kagrawal-2.attlocal.net>
2019-01-18 21:07:31 -0800

    First Push

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD7otyPOYToKhJWwT3P-fEc0shVf7_5Z0c",
+  authDomain: "productivityapp-256ef.firebaseapp.com",
+  databaseURL: "https://productivityapp-256ef.firebaseio.com",
+  projectId: "productivityapp-256ef",
+  storageBucket: "productivityapp-256ef.appspot.com",
+  messagingSenderId: "716347924908"
+};
