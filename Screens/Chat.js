592986ec5b0ae0197dab87268568fc0672253b27
Sidharth Naik <sidharthnaik@eduroam-169-233-248-225.ucsc.edu>
2019-02-11 21:15:41 -0800

    chat added

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -3,17 +3,29 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import { Header, Left, Right, Icon, Body, Title } from "native-base";
 import { LinearGradient } from "expo";
 import { Dimensions } from "react-native";
+import {GiftedChat} from "react-native-gifted-chat";
+import chatBackend from "./chatBackend";
+import firebase from "firebase";
 
 var width = Dimensions.get("window").width; //full width
 var height = Dimensions.get("window").height; //full height
 
 class Chat extends Component {
+  state = {
+    messages: []
+  };
   static navigationOptions = {
     header: null,
     drawerIcon: ({ tintColor }) => (
       <Icon name="ios-chatbubbles" style={{ fontSize: 24, color: tintColor }} />
     )
   };
+  user= firebase.auth().currentUser;
+
+  componentWillMount() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -34,8 +46,32 @@ class Chat extends Component {
           </Body>
           <Right />
         </Header>
+          <GiftedChat
+              messages={this.state.messages}
+              onSend={(message) => {
+                  chatBackend.sendMessage(message);
+              }}
+              user={{
+                _id: this.user.uid,
+                name: this.user.displayName,
+              }}
+          />
       </View>
     );
+  }
+
+  componentDidMount() {
+    chatBackend.loadMessages((message) =>{
+      this.setState((previousState) =>{
+        return {
+          messages: GiftedChat.append(previousState.messages,message),
+        };
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    chatBackend.closeChat();
   }
 }
 export default Chat;
---
+++ Screens/chatBackend.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase';
+
+class chatBackend {
+    uid = '';
+    messagesRef = null;
+
+    setUid(value) {
+        this.uid = value;
+    }
+    getUid() {
+        return this.uid;
+    }
+
+    loadMessages(callback) {
+        this.messagesRef = firebase.database().ref('messages');
+        this.messagesRef.off();
+            const onReceive = (data) => {
+                const message = data.val();
+                callback({
+                    _id: data.key,
+                    text: message.text,
+                    createdAt: new Date(message.createdAt),
+                    user: {
+                        _id: message.user._id,
+                        name: message.user.name,
+                    },
+                });
+            };
+            this.messagesRef.limitToLast(20).on('child_added', onReceive);
+    }
+
+    sendMessage(message) {
+        for(let i = 0; i < message.length; i++){
+            this.messagesRef.push({
+                text: message[i].text,
+                user: message[i].user,
+                createdAt: firebase.database.ServerValue.TIMESTAMP,
+            });
+        }
+    }
+
+    closeChat() {
+        if(this.messagesRef){
+            this.messagesRef.off();
+        }
+    }
+}
+
+export default new chatBackend();
+
